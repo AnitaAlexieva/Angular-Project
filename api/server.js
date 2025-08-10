0250cc4a3f93fd5f9dce89dd0147260e8a971c10
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-08-10 23:32:09 +0300

    Add an example server.js

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const clothesRoutes = require('./routes/clothes');
+const authRoutes = require('./routes/auth');
+app.use('/api/clothes', clothesRoutes);
+app.use('/api/auth', authRoutes);
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('MongoDB connected');
+    app.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    });
+  })
+  .catch(err => console.log(err));
