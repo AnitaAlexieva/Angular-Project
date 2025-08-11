80ff22d187725dfe73e8c3d4bef580d1b5d5afe0
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-08-11 15:14:48 +0300

    Create an example components and header with routing

--- new-app/src/app/app-routing.module.ts
+++ new-app/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { MyItemsComponent } from './pages/my-items/my-items.component';
+import { CreateComponent } from './pages/create/create.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'catalog/:id', component: DetailsComponent },
+  { path: 'favorites', component: FavoritesComponent },
+  { path: 'my-items', component: MyItemsComponent },
+  { path: 'create', component: CreateComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- new-app/src/app/app.module.ts
+++ new-app/src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { DetailsComponent } from './pages/details/details.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { CreateComponent } from './pages/create/create.component';
+import { FavoritesComponent } from './pages/favorites/favorites.component';
+import { MyItemsComponent } from './pages/my-items/my-items.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CatalogComponent,
+    DetailsComponent,
+    LoginComponent,
+    RegisterComponent,
+    CreateComponent,
+    FavoritesComponent,
+    MyItemsComponent,
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
