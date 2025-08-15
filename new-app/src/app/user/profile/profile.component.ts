fb0a94bc627be6bed4c72e9610abd8f4295de182
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-08-15 16:21:19 +0300

    Add basic edit profile functionality with edit form validation

--- new-app/src/app/user/profile/profile.component.ts
+++ new-app/src/app/user/profile/profile.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+  import { Component } from '@angular/core';
+  import { NgForm } from '@angular/forms';
+import { DOMAINS } from 'src/app/constants';
+  import { User } from 'src/app/types/User';
 
-@Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
-})
-export class ProfileComponent {
+  @Component({
+    selector: 'app-profile',
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
+  })
+  export class ProfileComponent {
+  domains=DOMAINS;
+  showEditMode: boolean = false;
 
+  profileDetails: User = {
+    username: 'Anitaaa',
+    email: 'anitaa@gmail.com',
+    imageUrl: 'https://i.pravatar.cc/150?img=12'
+  };
+
+  formModel: User = { ...this.profileDetails }; // копие за формата
+
+  onEdit(): void {
+    this.showEditMode = true;
+    this.formModel = { ...this.profileDetails }; // reset на формата
+  }
+
+  saveProfile(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.profileDetails = { ...this.formModel }; // записваме само при Save
+    this.showEditMode = false;
+  }
+
+  cancelEdit() {
+    this.showEditMode = false;
+  }
 }
