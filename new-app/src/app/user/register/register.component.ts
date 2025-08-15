fa4ae6c3d08e24ead3a938b678852fb4373ce27b
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-08-15 14:15:08 +0300

    Display error for every input in register form when there is invalid data

--- new-app/src/app/user/register/register.component.ts
+++ new-app/src/app/user/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DOMAINS } from 'src/app/constants';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+    domains = DOMAINS;
+    constructor(
+      private userService: UserService,
+      private router: Router
+    ){}
 
+    register(form:NgForm){
+      if(form.invalid){
+        return;
+      }
+
+      console.log('Само стойностите:', form.value);
+    }
 }
